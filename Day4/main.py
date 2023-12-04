fab7ae5c0d8b44a59cd3914acab1f740e6aaa666
Mats <49249061+Matseslats@users.noreply.github.com>
2023-12-04 11:50:32 +0000

    Day 4 Pt 2

--- Day4/main.py
+++ Day4/main.py
@@ -5,10 +5,7 @@ def score_match(win_nums: List[int], my_nums: List[int]) -> int:
     for num in my_nums:
         nums_correct += num in win_nums
     
-    # Get the score, 1 match = 1 pt, double for every next match
-    if nums_correct <= 0:
-        return 0
-    return pow(2, nums_correct-1)
+    return nums_correct
 
 def get_ints(in_str: str) -> List[int]:
     strs = in_str.split(" ")
@@ -20,15 +17,29 @@ def get_ints(in_str: str) -> List[int]:
 
 def get_score(line: str) -> int:
     print(line)
+    # win_nums = get_ints(line[8:22])
+    # my_nums = get_ints(line[25:])
     win_nums = get_ints(line[10:39])
     my_nums = get_ints(line[42:])
     score = score_match(win_nums, my_nums)
-    print(win_nums, my_nums, score)
+    # print(win_nums, my_nums, score)
     return score
 
 
 def get_scores(lines: List[str]) -> List[int]:
-    return [get_score(line) for line in lines]
+    tot_cards = 0
+    card_copies = [1]*len(lines)
+    print(card_copies)
+    for i, line in enumerate(lines):
+        # Get how many cards you won, card_copies[i] is how many were played
+        # print(card_copies)
+        won_cards = get_score(line)
+        # Add the amount of cards you won (the amont of this card you have) to the next n, num of cards won, cards
+        for j in range(i+1, i+1+won_cards):
+            card_copies[j] += card_copies[i]
+        # print(won_cards, card_copies[i])
+        tot_cards += card_copies[i]
+    return tot_cards
 
 if __name__ == "__main__":
     lines = []
@@ -38,6 +49,6 @@ if __name__ == "__main__":
     
     part_nums = get_scores(lines)
 
-    sum = sum(part_nums)
+    sum = part_nums
 
     print(f"Sum: {sum}")
