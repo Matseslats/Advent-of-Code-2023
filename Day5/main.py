74f3a91254545e61fea54c69381f5cd0465509fa
Mats <49249061+Matseslats@users.noreply.github.com>
2023-12-05 13:33:00 +0000

    Optimized speed

--- Day5/main.py
+++ Day5/main.py
@@ -1,9 +1,9 @@
 from typing import List
 
 def empty_map() -> dict:
-    map = {}
-    for i in range(100):
-        map[i] = i
+    map = {
+        "conversions": []
+    }
 
     return map
 
@@ -26,7 +26,6 @@ def get_maps(lines: List[str]) -> dict:
 
         if line == "":
             # Empty line, save map to maps
-            print(current_map)
             maps[map_name] = current_map
             current_map = empty_map()
             new_map = True
@@ -34,17 +33,21 @@ def get_maps(lines: List[str]) -> dict:
 
         # Parse map line
         dest_start, source_start, range_len = [int(x) for x in line.split(" ")]
-        for i in range(range_len):
-            current_map[source_start+i] = dest_start+i
-
-        print(line)
+        current_map["conversions"].append((dest_start, source_start, range_len))
     
-    print(current_map)
     maps[map_name] = current_map
     current_map = empty_map()
     new_map = True
     
     return maps
+
+def get_index_from_map(this_map, index):
+    # Search though conversion ranges for this index
+    for conversion in this_map["conversions"]:
+        if index in range(conversion[1], conversion[1]+conversion[2]):
+            return (index + conversion[0] - conversion[1])
+    # None found, just return the index. It is not mapped to anything
+    return index
 
 if __name__ == "__main__":
     lines = []
@@ -56,17 +59,19 @@ if __name__ == "__main__":
 
     min_location = 100000000000000
     for seed in maps["to_convert"]:
-        soil = maps["seed-to-soil"][seed]
-        fertilizer = maps["soil-to-fertilizer"][soil]
-        water = maps["fertilizer-to-water"][fertilizer]
-        light = maps["water-to-light"][water]
-        temperature = maps["light-to-temperature"][light]
-        humidity = maps["temperature-to-humidity"][temperature]
-        location = maps["humidity-to-location"][humidity]
+        soil = get_index_from_map(maps["seed-to-soil"], seed)
+        fertilizer = get_index_from_map(maps["soil-to-fertilizer"], soil)
+        # soil = maps["seed-to-soil"][seed]
+        # fertilizer = maps["soil-to-fertilizer"][soil]
+        water = get_index_from_map(maps["fertilizer-to-water"], fertilizer)
+        light = get_index_from_map(maps["water-to-light"], water)
+        temperature = get_index_from_map(maps["light-to-temperature"], light)
+        humidity = get_index_from_map(maps["temperature-to-humidity"], temperature)
+        location = get_index_from_map(maps["humidity-to-location"], humidity)
 
         min_location = min(min_location, location)
 
-        print(location)
+        print(seed, location)
     
     print(min_location)
 
