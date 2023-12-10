2814add54dbad495561e2b3e77c42d0c4d859c19
Mats <49249061+Matseslats@users.noreply.github.com>
2023-12-10 15:40:36 +0000

    Day 10 Part 2

--- Day10/main.py
+++ Day10/main.py
@@ -1,9 +1,16 @@
+from copy import deepcopy
 from dataclasses import dataclass
+from enum import Enum
 import time
 from typing import List
 from PIL import Image, ImageColor
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
+import sys
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+sys.setrecursionlimit(15000)
 
 @dataclass
 class graphVertex:
@@ -14,6 +21,9 @@ class graphVertex:
     parent = None
     coords = None
     used_in_max_depth = False
+    visited = False
+    potentially_enclosed = False
+    child = None
     def __init__(self, symbol: str):
         self.symbol = symbol
         self.distance = None
@@ -21,6 +31,10 @@ class graphVertex:
     
     def __str__(self):
         return f'{self.symbol}'
+
+    def setPotentially_enclosed(self, bool):
+        if not self.visited:
+            self.potentially_enclosed = True
 
 def is_in_range(coord, max_w, max_h):
     x, y = coord
@@ -147,6 +161,8 @@ def png_graph(graph, width, height, max):
                     color = generate_color_from_gradient(node.distance, 0, max, cmap)
                     if not node.used_in_max_depth:
                         color = (color[0], color[1], color[2], 50)
+            # if len(node.out_edges) == 0:
+            #     color = (100,100,100,255)
             im.putpixel((x, y), color)  # directly use the RGB tuple
 
     im.putpixel(graph["start"], (255,0,0,255))
@@ -159,25 +175,94 @@ def png_graph(graph, width, height, max):
 
     im.save('simplePixel.png') # or any image format
 
+# Go through the graph clockwise, set all nodes on starboard (if not visited) as potentially enclosed
+def png_dfs_graph(graph, width, height, max_depth, out="part2.png"):
+    cmap = create_nipy_spectral_cmap()
+    im = Image.new('RGBA', (width,height)) # create the Image of size 1 pixel 
+    for y in range(graph["height"]):
+        for x in range(graph["width"]):
+            color = (0, 0, 0,0)
+            node = graph["nodes"].get((x, y))
+            if node is not None:
+                if node.visited:
+                    color = generate_color_from_gradient(node.distance, -1, max_depth, cmap)
+                    # color = (150,150,150,255)
+                    # color = (255,255,255,255)
+                    # if not node.used_in_max_depth:
+                    #     color = (color[0], color[1], color[2], 50)
+            if len(node.out_edges) == 0:
+                color = (0,0,0,255)
+            # if node.potentially_enclosed:
+            #     color = (200, max(color[1]-100,0), max(color[2]-100,0), color[3])
+            im.putpixel((x, y), color)  # directly use the RGB tuple
+
+    # im.putpixel(graph["start"], (0,255,0,255))
+    # im.putpixel(graph["farthest"], (0,0,255,255))
+
+    # v = graph["nodes"][graph["farthest"]]
+    # while v is not None:
+    #     im.putpixel(v.coords, (255,255,255,255))
+    #     v = v.parent
+
+    im.save(out) # or any image format
+
+
+def dfs(graph, u, depth=0, nodes=[]):
+    for coord in u.out_edges:
+        v = graph["nodes"][coord]
+        if v.visited == False:
+            nodes.append(v.coords)
+            v.potentially_enclosed = False
+            v.parent = u
+            u.child = v
+            v.visited = True
+            v.distance = depth
+                
+            graph, depth, nodes = dfs(graph, v, depth+1, nodes)
+
+    return graph, depth, nodes
+
+def polygonArea(vertices):
+    #A function to apply the Shoelace algorithm
+    numberOfVertices = len(vertices)
+    sum1 = 0
+    sum2 = 0
+
+    for i in range(0,numberOfVertices-1):
+        sum1 = sum1 + vertices[i][0] *  vertices[i+1][1]
+        sum2 = sum2 + vertices[i][1] *  vertices[i+1][0]
+
+    #Add xn.y1
+    sum1 = sum1 + vertices[numberOfVertices-1][0]*vertices[0][1]   
+    #Add x1.yn
+    sum2 = sum2 + vertices[0][0]*vertices[numberOfVertices-1][1]   
+
+    area = abs(sum1 - sum2) / 2
+    return area
 
 if __name__ == "__main__":
     start_time = time.time()
     lines = []
-    with open("input.txt") as file:
+    with open("maininput.txt") as file:
         lines = file.readlines()
         lines = [line.strip() for line in lines]
     
     graph = get_graph(lines)
 
-    # print_graph(graph)
+    original_graph = deepcopy(graph)
 
-    max_depth = bfs(graph)
+    bfs_graph = deepcopy(graph)
+    max_depth = bfs(bfs_graph)
+    png_graph(bfs_graph, graph["width"], graph["height"], max_depth)
 
-    # print_graph(graph, dist=True)
 
-    png_graph(graph, graph["width"], graph["height"], max_depth)
+    dfs_graph, max_length, nodes = dfs(deepcopy(graph), graph["nodes"][graph["start"]])
+    png_dfs_graph(dfs_graph, graph["width"], graph["height"], max_length)
+
+    polygon = Polygon(nodes)
+
 
 
     end_time = time.time()
-    print(f"Max depth: {max_depth}")
+    print(f"Max depth: {max_depth}. Max length: {max_length}. Contains {polygon.area - max_length/2 + 1} points")
     print(f"Took {((end_time-start_time)*1000):.4}ms")
