199fc6827a3339d1f01a6feece2e4485a1116898
Mats <49249061+Matseslats@users.noreply.github.com>
2023-12-11 13:14:17 +0000

    Day 11 Pt 2 Attempt

--- Day11/main.py
+++ Day11/main.py
@@ -3,31 +3,39 @@ from typing import List
 from tqdm import tqdm
 
 # Expand empty lines
-def expand_galaxies(lines: List[bool]):
+def expand_galaxies(lines: List[bool], scale=1):
     # print("Y pre:", len(lines))
+    expand_x = {}
+    expand_y = {}
     index = 0
+    print("Expanding y")
     for y, line in enumerate(lines):
         if not any(line):
-            # print(f"No galaxies at y={index}", line)
-            lines = lines[:index] + [line] + lines[index:]
-            index += 1
-        index += 1
+            expand_y[y] = scale
+        #     # print(f"No galaxies at y={index}", line)
+        #     lines = lines[:index] + [line] + lines[index:]
+        #     index += 1
+        # index += 1
     # print("Y post:", len(lines))
     
     # print()
 
     # print("X pre:", len(lines[0]))
     index = 0
+    print("Expanding x")
     for x, _ in enumerate(lines[0]):
-        line = [line[index] for line in lines]
+        line = [line[x] for line in lines]
         if not any(line):
-            # print(f"No galaxies at x={index}", line)
-            lines = [line[:index] + [line[index]] + line[index:] for line in lines]
-            index += 1
-        index += 1
+            expand_x[x] = scale
+            # for i in range(scale):
+            #     # print(f"No galaxies at x={index}", line)
+            #     lines = [line[:index] + [line[index]] + line[index:] for line in lines]
+            #     index += 1
+        # index += 1
     # print("X post:", len(lines[0]))
         
-    return lines
+    print(expand_x, expand_y)
+    return lines, expand_x, expand_y
 
 # Get coords of where galaxies are
 def get_galaxy_coords(lines: List[bool]) -> List[tuple]:
@@ -38,16 +46,30 @@ def get_galaxy_coords(lines: List[bool]) -> List[tuple]:
                 out.append((x,y))
     return out
 
-def get_dist(coord: tuple, coord2: tuple) -> int:
-    return abs(abs(coord[0]-coord2[0]) + abs(coord[1] - coord2[1]))
 
-def get_pairs(coords: List[tuple]) -> List[List[tuple]]:
+# print("8->9",get_dist((0,11),(5,11), {2: 1, 5: 1, 8: 1}, {3: 1, 7: 1})) = 5
+def get_dist(coord: tuple, coord2: tuple, expand_x: dict, expand_y: dict) -> int:
+    dist = abs(abs(coord[0]-coord2[0]) + abs(coord[1] - coord2[1]))
+    # print(dist)
+    for check_x in range(coord[0], coord2[0]):
+        if expand_x.get(check_x) is not None:
+            # print("Adding")
+            dist += expand_x[check_x]
+    
+    for check_y in range(coord[1], coord2[1]):
+        if expand_y.get(check_y) is not None:
+            # print("Adding")
+            dist += expand_y[check_y]
+
+    return dist
+
+def get_pairs(coords: List[tuple], expand_x: dict, expand_y: dict) -> List[List[tuple]]:
     out = []
     checked = {}
     print("Getting pairs")
     for coord in tqdm(coords):
         for coord2 in coords:
-            dist = get_dist(coord, coord2)
+            dist = get_dist(coord, coord2, expand_x, expand_y)
             if coord != coord2:
                 if not ((coord,coord2) in checked or (coord2,coord) in checked):
                     out.append((dist, coord, coord2))
@@ -65,11 +87,11 @@ def print_galaxies(lines: List[List[bool]])->None:
 
 # Get the results needes
 def get_galaxies(lines: List[str]) -> int:
-    # print_galaxies(lines)
-    lines = expand_galaxies(lines)
+    print_galaxies(lines)
+    lines, expand_x, expand_y = expand_galaxies(lines, 1)
     # print_galaxies(lines)
     coords = get_galaxy_coords(lines)
-    pairs = get_pairs(coords)
+    pairs = get_pairs(coords, expand_x, expand_y)
     return coords, pairs
 
 if __name__ == "__main__":
@@ -88,10 +110,10 @@ if __name__ == "__main__":
     for d in pairs:
         sum_of_dists += d[0]
 
-    print("1->7",get_dist((4,0),(9,10)))
-    print("3->6",get_dist((0,2),(12,7)))
-    print("8->9",get_dist((0,11),(5,11)))
-    print("1->2",get_dist((4,0),(9,1)))
+    # print("1->7",get_dist((3,0),(7,8), {2: 1, 5: 1, 8: 1}, {3: 1, 7: 1}))
+    # print("3->6",get_dist((0,2),(9,6), {2: 1, 5: 1, 8: 1}, {3: 1, 7: 1}))
+    # print("8->9",get_dist((0,10),(4,10), {2: 1, 5: 1, 8: 1}, {3: 1, 7: 1}))
+    # print("1->2",get_dist((3,0),(7,1), {2: 1, 5: 1, 8: 1}, {3: 1, 7: 1}))
 
 
     end_time = time.time()
