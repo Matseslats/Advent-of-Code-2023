8da8e64f8b2590c64e3f8683e51ea2957b2aea87
Mats <49249061+Matseslats@users.noreply.github.com>
2023-12-13 13:57:30 +0000

    Day 13 Pt 2

--- Day13/main.py
+++ Day13/main.py
@@ -1,12 +1,17 @@
 import time
 import copy
 
+time_spent_copying_array = 0
+
 def get_error_fixed_reflection(lines):
+    global time_spent_copying_array
     val_to_avoid = get_reflection(lines)
     # print(f"Do not return if {val_to_avoid}")
     for y,_ in enumerate(lines):
         for x,_ in enumerate(lines[0]):
+            s = time.time()
             mutable_lines = copy.deepcopy(lines)
+            time_spent_copying_array += time.time()-s
             mutable_lines[y][x] = not lines[y][x]
             # print(f"Mutating y={y} x={x}")
             result = get_reflection(mutable_lines, avoid=val_to_avoid)
@@ -26,7 +31,8 @@ def get_reflection(lines, avoid=None):
         line_comp = min(y, len(lines)-y)
         if lines[(y-line_comp):y] == list(reversed(lines[y:y+line_comp])):
             # print(f"Symmetry at y:{y}")
-            if avoid is None or y!= avoid:
+            if not (avoid is not None and y*100 == avoid):
+                # print(avoid, y)
                 return (y)*100
 
     for x,_ in enumerate(lines[0]):
@@ -35,7 +41,8 @@ def get_reflection(lines, avoid=None):
         line_comp = min(x, len(lines[0])-x)
         if all([list(line[(x-line_comp):x]) == list(reversed(line[x:x+line_comp])) for line in lines]):
             # print(f"Symmetry at x:{x}")
-            if avoid is None or y != avoid:
+            if not (avoid is not None and x == avoid):
+                # print(avoid, x)
                 return x
 
 
@@ -52,14 +59,12 @@ if __name__ == "__main__":
     for line in lines:
         if line == []:
             value += get_error_fixed_reflection(to_test)
-            # print("New val", value)
             to_test = []
         else:
             to_test.append(line)
-
-    # value = get_reflection(lines)
 
 
     end_time = time.time()
     print(f"Solution: {value}")
     print(f"Took {((end_time-start_time)*1000):.4}ms")
+    print(f"Time spent in deepcopy {((time_spent_copying_array)*1000):.4}ms")
