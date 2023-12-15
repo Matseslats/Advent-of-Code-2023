103b272f7b4afd73ec4327abb21ecf82290457e9
Mats <49249061+Matseslats@users.noreply.github.com>
2023-12-15 10:33:29 +0000

    Day 15 Pt 2

--- Day15/main.py
+++ Day15/main.py
@@ -1,4 +1,30 @@
 import time
+from collections import OrderedDict
+import re
+
+def get_focal_power(boxes):
+    sum = 0
+    for key in boxes:
+        for index, inner_key in enumerate(boxes[key]):
+            sum += (key+1) * (index+1) * boxes[key][inner_key]
+    return sum
+
+
+def update_box(in_arr, boxes):
+    # SPlit at either = or -
+    parts = re.split(r'[-=]', in_arr)
+    value = None
+    hashval = get_hash(parts[0])
+    if parts[1] != '':
+        value = int(parts[1])
+        if hashval not in boxes:
+            boxes[hashval] = OrderedDict()
+        boxes[hashval][parts[0]] = value
+    else:
+        if hashval in boxes and parts[0] in boxes[hashval]:
+            del boxes[hashval][parts[0]]
+    
+    return boxes
 
 def get_hash(in_arr):
     hash = 0
@@ -19,6 +45,13 @@ if __name__ == "__main__":
 
     hashes = [get_hash(line) for line in in_array]
 
+    boxes = {}
+    for part in in_array:
+        boxes = update_box(part, boxes)
+    
+    focal_power = get_focal_power(boxes)
+
     end_time = time.time()
     print(f"Solution Pt1: {sum(hashes)}")
+    print(f"Solution Pt2: {focal_power}")
     print(f"Took {((end_time - start_time) * 1000):.4}ms")
