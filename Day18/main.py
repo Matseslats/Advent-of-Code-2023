899249e93cbd7a308a219020d30ec6a52e6f2885
Mats <49249061+Matseslats@users.noreply.github.com>
2023-12-18 05:41:06 +0000

    Day 18 Pt 2

--- Day18/main.py
+++ Day18/main.py
@@ -19,7 +19,7 @@ def count_squares_in_path(path):
     return total_squares
 
 
-def solve(lines: List[str]):
+def solve(lines: List[str], useColor=False):
     x, y = 0, 0
     max_x, max_y = 0, 0
     nodes = {(x, y): 0}
@@ -27,18 +27,21 @@ def solve(lines: List[str]):
     perimeter = 0
     area = 0
     dist_vecs = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+    color_vecs = {"3": (-1, 0), "1": (1, 0), "2": (0, -1), "0": (0, 1)}
     for l in lines:
         direc, length, color = l.split(" ")
+        color = color[2:-1]
         length = int(length)
         dy, dx = dist_vecs[direc]
+        if useColor:
+            # Pt 2
+            length = int(color[:-1],16)
+            dy, dx = color_vecs[color[-1]]
+
         dy, dx = dy*length, dx*length
         y, x = y+dy, x+dx
         perimeter+=length
         area+=x*dy
-        print(length)
-
-    print(area, perimeter)
-    print(area+perimeter//2+1)
 
     return area+perimeter//2+1
 
@@ -49,9 +52,11 @@ if __name__ == "__main__":
         lines = [line.strip() for line in file.readlines()]
 
     start_time = time.time()
-    loss_pt1 = solve(lines)
+    loss_pt1 = solve(lines, useColor=False)
+    loss_pt2 = solve(lines, useColor=True)
 
     end_time = time.time()
     print(f"Solution Pt1: {loss_pt1}")
+    print(f"Solution Pt2: {loss_pt2}")
     # print(f"Solution Pt2: {max_energized}")
     print(f"Took {((end_time - start_time) * 1000):.4}ms")
