478734c9868e6d4d8aed15ec96bfb755458f94cb
Mats <49249061+Matseslats@users.noreply.github.com>
2023-12-21 14:31:19 +0000

    Generalized pt 1. Towards pt 2

--- Day21/main.py
+++ Day21/main.py
@@ -8,16 +8,46 @@ def init_graph(lines):
             graph[(x,y)] = []
             if x > 0 and lines[y][x-1] in ".S": # Left
                 graph[(x,y)].append((x-1,y))
+            elif x == 0 and lines[y][-1] in ".S":
+                graph[(x,y)].append((len(lines[y])-1,y))
             if x < len(lines[y])-1 and lines[y][x+1] in ".S": # Right
                 graph[(x,y)].append((x+1,y))
+            elif x == len(lines[y])-1 and lines[y][0] in ".S":
+                graph[(x,y)].append((0,y))
             if y > 0 and lines[y-1][x] in ".S": # Up
                 graph[(x,y)].append((x,y-1))
+            elif y == 0 and lines[-1][x] in ".S":
+                graph[(x,y)].append((x,len(lines)-1))
             if y < len(lines)-1 and lines[y+1][x] in ".S": # Down
                 graph[(x,y)].append((x,y+1))
+            elif y == len(lines)-1 and lines[0][x] in ".S":
+                graph[(x,y)].append((x,0))
             
             if char == "S":
                 graph["S"] = (x,y)
     return graph
+
+
+def print_dists(distances, lines):
+    for y,_ in enumerate(lines):
+        for x,char in enumerate(lines[y]):
+            if char == "#":
+                print("    .", end="")
+            elif char == "S":
+                print("    S", end="")
+            elif (x,y) in distances:
+                print(f"{distances[(x,y)]:5}", end="")
+            else:
+                print("    -", end="")
+        print()
+
+
+def get_dist(distances, dist):
+    count = 0
+    for space in distances:
+        if distances[space] <= dist and (dist % 2 == distances[space] % 2):
+            count += 1
+    return count
 
 
 if __name__ == "__main__":
@@ -28,19 +58,23 @@ if __name__ == "__main__":
     start_time = time.time()
 
     graph = init_graph(lines)
+    distances = {graph["S"]: 0}
     current_spaces = set()
     current_spaces.add(graph["S"])
-    for i in tqdm.tqdm(range(64)):
+    for i in tqdm.tqdm(range(240)):
         new_spaces = set()
         for space in current_spaces:
             for neighbor in graph[space]:
+                if neighbor not in distances:
+                    distances[neighbor] = distances[space] + 1
                 new_spaces.add(neighbor)
         
         current_spaces = new_spaces
-    # print(current_spaces)
+
+    print_dists(distances, lines)
 
     pt1 = pt2 = 0
-    pt1 = len(current_spaces)
+    pt1 = get_dist(distances, 64)
 
     end_time = time.time()
     print(f"Solution Pt1: {pt1}")
